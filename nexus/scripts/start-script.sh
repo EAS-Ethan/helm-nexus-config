d736dbb945b6bbf84db5f5fd1ffeeeec90caa02e
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-05 11:25:18 +0100

    change

--- nexus/scripts/start-script.sh
+++ nexus/scripts/start-script.sh
@@ -1,4 +1,8 @@
 cd /opt/sonatype/nexus
+echo "lol"
+# we might have to put some logic here to wait
+# for nexus container to start and create filesystem
+
 exec ./bin/nexus run & \
     sh /scripts/healthcheck.sh && \
     /scripts/provision.sh
