a712e341b99fcd4de99ef7948d18db927fadcf6f
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-06 14:03:48 +0100

    change

--- nexus/scripts/shell/start-nexus-repository-manager.sh
+++ nexus/scripts/shell/start-nexus-repository-manager.sh
@@ -4,8 +4,9 @@ exec yes | dnf install unzip
 exec yes | dnf install zip
 exec sh /config/shell/install-groovy.sh
 
-source "/root/.sdkman/bin/sdkman-init.sh"
-sdk install groovy
+
+exec source "/root/.sdkman/bin/sdkman-init.sh" && \
+    sdk install groovy
 exec ./bin/nexus run & \
     sh /config/shell/healthcheck.sh && \
     /config/shell/provision.sh
