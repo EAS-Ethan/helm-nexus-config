303256e2471825d37473d046e38d19b4cae8438b
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-06 14:25:55 +0100

    change

--- nexus/scripts/shell/install-groovy.sh
+++ nexus/scripts/shell/install-groovy.sh
@@ -5,4 +5,4 @@ exec curl -s get.sdkman.io | bash &&  \
     chmod a+x "/root/.sdkman/bin/sdkman-init.sh"  && \
     source "/root/.sdkman/bin/sdkman-init.sh" && \
     echo "sdkman_auto_answer=true" > ~/.sdkman/etc/config && \
-    sdk install groovy &
+    # sdk install groovy &
