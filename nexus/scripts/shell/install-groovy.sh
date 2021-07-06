577bc74eaa2da705127cd77e344063427c52ecde
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-06 13:59:51 +0100

    change

--- nexus/scripts/shell/install-groovy.sh
+++ nexus/scripts/shell/install-groovy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 
-exec cd /opt/sonatype/nexus & \
-    curl -s get.sdkman.io | bash &&  \
+exec curl -s get.sdkman.io | bash &&  \
     chmod a+x "/root/.sdkman/bin/sdkman-init.sh"  && \
     source "/root/.sdkman/bin/sdkman-init.sh" && \
-    sleep 20
+    sdk install groovy 
--- nexus/scripts/shell/start-nexus-repository-manager.sh
+++ nexus/scripts/shell/start-nexus-repository-manager.sh
@@ -1,10 +1,8 @@
-cd /opt/sonatype/nexus
-exec yes | dnf update bash
+#!/bin/bash
+
 exec yes | dnf install unzip
 exec yes | dnf install zip
-exec bash --version & \
-    sh /config/shell/install-groovy.sh && \
-    sdk install groovy && \
+exec sh /config/shell/install-groovy.sh && \
     ./bin/nexus run & \
     sh /config/shell/healthcheck.sh && \
     /config/shell/provision.sh
