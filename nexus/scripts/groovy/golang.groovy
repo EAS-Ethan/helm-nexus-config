12899d4a84832ee84adab115dee610f71bc23024
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-06 17:34:04 +0100

    change

--- nexus/scripts/groovy/golang.groovy
+++
@@ -1,6 +0,0 @@
-import org.sonatype.nexus.repository.storage.WritePolicy
-import org.sonatype.nexus.blobstore.api.BlobStoreManager
-
-repository.createGolangHosted("Golang-hosted", "default", true, WritePolicy.ALLOW_ONCE)
-repository.createGolangProxy("Golang-proxy", "https://gonexus.dev", BlobStoreManager.DEFAULT_BLOBSTORE_NAME)
-repository.createGolangGroup("Golang-group", ["Golang-hosted", "Golang-proxy"], "default")
---
+++ nexus/scripts/groovy/nuget.groovy
@@ -0,0 +1,15 @@
+import org.sonatype.nexus.blobstore.api.BlobStoreManager
+import org.sonatype.nexus.repository.config.WritePolicy
+
+// create a nuget repository backed by the default blob store.
+repository.createNugetHosted("nuget-hosted", BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true, WritePolicy.ALLOW  )
+// create a nuget proxy repository backed by the default blob store.
+// see https://help.sonatype.com/display/NXRM3/Node+Packaged+Modules+and+npm+Registries
+createNugetProxy("nuget-proxy","https://api.nuget.org/v3/index.json" ,BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true)
+// create a npm group repository that merges the npm-host and npmjs.org-proxy together.
+// name - The name of the new Repository
+// remoteUrl - The url of the external proxy for this Repository
+// blobStoreName - The BlobStore the Repository should use
+// strictContentTypeValidation - Whether or not the Repository should enforce strict content types
+
+repository.createNugetGroup("npm-group", ["nuget-hosted", "nuget-proxy"], BlobStoreManager.DEFAULT_BLOBSTORE_NAME)
