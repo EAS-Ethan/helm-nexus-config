7dfe109606f020cc2cd99dd81ee9742aba21e62c
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-06 14:19:29 +0100

    change

--- nexus/scripts/shell/start-nexus-repository-manager.sh
+++ nexus/scripts/shell/start-nexus-repository-manager.sh
@@ -3,8 +3,8 @@
 
 exec yes | dnf install unzip
 exec yes | dnf install zip
-exec bash /config/shell/install-groovy.sh & \
-    ./bin/nexus run & \
+exec bash /config/shell/install-groovy.sh
+exec ./bin/nexus run & \
     sh /config/shell/healthcheck.sh && \
     /config/shell/provision.sh
 
