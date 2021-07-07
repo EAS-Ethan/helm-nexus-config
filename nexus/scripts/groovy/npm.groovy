6278b26e87808f5f40730fc7562458d6fa7288d2
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-07 12:21:31 +0100

    change

--- nexus/scripts/groovy/npm.groovy
+++ nexus/scripts/groovy/npm.groovy
@@ -1,11 +1,8 @@
-
 import org.sonatype.nexus.capability.CapabilityRegistry
 import org.sonatype.nexus.repository.storage.WritePolicy
 
-// create a npm repository backed by the default blob store.
 repository.createNpmHosted("npm-hosted",  BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true, WritePolicy.ALLOW_ONCE)
-// create a npm proxy repository backed by the default blob store.
-// see https://help.sonatype.com/display/NXRM3/Node+Packaged+Modules+and+npm+Registries
+
 repository.createNpmProxy("npmjs.org-proxy", "https://registry.npmjs.org",  BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true)
-// create a npm group repository that merges the npm-host and npmjs.org-proxy together.
+
 repository.createNpmGroup("npm-group", ["npm-hosted", "npmjs.org-proxy"],  BlobStoreManager.DEFAULT_BLOBSTORE_NAME)
