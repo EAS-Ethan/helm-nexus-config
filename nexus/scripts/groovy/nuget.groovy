3491a5ad8eda5ba481e9bc30866aa413374d3878
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-07 11:08:10 +0100

    change

--- nexus/scripts/groovy/nuget.groovy
+++ nexus/scripts/groovy/nuget.groovy
@@ -2,10 +2,10 @@ import org.sonatype.nexus.blobstore.api.BlobStoreManager
 import org.sonatype.nexus.repository.config.WritePolicy
 
 // create a nuget repository backed by the default blob store.
-repository.createNugetHosted("nuget-hosted", BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true, WritePolicy.ALLOW  )
+repository.createNugetHosted("nuget-hosted", BlobStoreManager.DEFAULT_BLOBSTORE_NAME, false, WritePolicy.ALLOW)
 // create a nuget proxy repository backed by the default blob store.
 // see https://help.sonatype.com/display/NXRM3/Node+Packaged+Modules+and+npm+Registries
-createNugetProxy("nuget-proxy","https://api.nuget.org/v3/index.json" ,BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true)
+createNugetProxy("nuget-proxy","https://api.nuget.org/v3/index.json", BlobStoreManager.DEFAULT_BLOBSTORE_NAME, false)
 // create a npm group repository that merges the npm-host and npmjs.org-proxy together.
 // name - The name of the new Repository
 // remoteUrl - The url of the external proxy for this Repository
