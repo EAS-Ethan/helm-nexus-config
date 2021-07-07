02fb3f8c588ad79b624004113e9b7d475b6240a8
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-07 16:58:15 +0100

    change

--- nexus/scripts/groovy/npm.groovy
+++ nexus/scripts/groovy/npm.groovy
@@ -1,9 +1,9 @@
-import org.sonatype.nexus.capability.CapabilityRegistry
+import org.sonatype.nexus.blobstore.api.BlobStoreManager
 //right
 import org.sonatype.nexus.repository.config.WritePolicy
 
 
-repository.createNpmHosted("npm-hosted",  BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true, WritePolicy.ALLOW)
+repository.createNpmHosted("npm-hosted",  BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true, WritePolicy.ALLOW_ONCE)
 
 repository.createNpmProxy("npm-prxy", "https://registry.npmjs.org",  BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true)
 
