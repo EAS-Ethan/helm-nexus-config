d8b0d3bdb186d6591f47b56b8d63e696251e2c4d
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-08 15:50:06 +0100

    change

--- nexus/scripts/groovy/nuget.groovy
+++ nexus/scripts/groovy/nuget.groovy
@@ -2,16 +2,10 @@ import org.sonatype.nexus.blobstore.api.BlobStoreManager
 import org.sonatype.nexus.repository.config.WritePolicy
 import org.sonatype.nexus.cleanup.storage.CleanupPolicyStorage
 
-// create a nuget repository backed by the default blob store.
+
 repository.createNugetHosted("nuget-hosted", BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true, WritePolicy.ALLOW_ONCE)
-// create a nuget proxy repository backed by the default blob store.
-// see https://help.sonatype.com/display/NXRM3/Node+Packaged+Modules+and+npm+Registries
+
 repository.createNugetProxy("nuget-proxy","https://api.nuget.org/v3/index.json", BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true)
-// create a npm group repository that merges the npm-host and npmjs.org-proxy together.
-// name - The name of the new Repository
-// remoteUrl - The url of the external proxy for this Repository
-// blobStoreName - The BlobStore the Repository should use
-// strictContentTypeValidation - Whether or not the Repository should enforce strict content types
 
 repository.createNugetGroup("nuget-group", ["nuget-hosted", "nuget-proxy"], BlobStoreManager.DEFAULT_BLOBSTORE_NAME)
 
@@ -49,8 +43,10 @@ def attachPolicy (policyName, repositoryName) {
 }
 
 createPolicy('nugetCleanupPolicy', 'nuget')
-sleep(1000)
+sleep(10000)
 attachPolicy('nugetCleanupPolicy', 'nuget-hosted')
+
+
 
 
 
