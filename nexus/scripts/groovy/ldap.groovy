030d09f1247d10b576d34bf8196c7c2a5a98bfe7
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-08 12:57:37 +0100

    change

--- nexus/scripts/groovy/ldap.groovy
+++ nexus/scripts/groovy/ldap.groovy
@@ -33,7 +33,7 @@ parsed_args = new JsonSlurper().parseText(args)
 
 def ldapConfigMgr = container.lookup(LdapConfigurationManager.class.getName());
 
-def ldapConfig = new LdapConfiguration()
+def ldapConfig = ldapConfigMgr.newConfiguration();
 boolean update = false;
 
 // Look for existing config to update
