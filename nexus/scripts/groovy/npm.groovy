b6b76911f854206ccce42629b4b595641756de86
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-08 13:44:06 +0100

    change

--- nexus/scripts/groovy/npm.groovy
+++ nexus/scripts/groovy/npm.groovy
@@ -7,3 +7,38 @@ repository.createNpmHosted("npm-hosted",  BlobStoreManager.DEFAULT_BLOBSTORE_NAM
 repository.createNpmProxy("npm-prxy", "https://registry.npmjs.org",  BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true)
 
 repository.createNpmGroup("npm-group", ["npm-hosted", "npm-prxy"],  BlobStoreManager.DEFAULT_BLOBSTORE_NAME)
+
+
+
+def createPolicy (npmPolicy) {
+    try {
+        def policyStorage = container.lookup(CleanupPolicyStorage.class.getName())
+        def cleanupPolicy = policyStorage.newCleanupPolicy()
+        cleanupPolicy.setName(npmPolicy)
+        cleanupPolicy.setNotes('')
+        cleanupPolicy.setMode('deletion')
+        cleanupPolicy.setFormat('npm')
+        cleanupPolicy.setCriteria(['regex': '.*SNAPSHOT'])
+        policyStorage.add(cleanupPolicy)
+    } catch (e) {
+        log.info("Cleanup policy already exists, skipping...")
+    }
+
+}
+
+def attachPolicy (policyName, repositoryName) {
+    try {
+        def repo = repository.repositoryManager.get(repositoryName)
+        def cleanupPolicyAttribute = [policyName: [policyName].toSet()]
+        def conf = repo.getConfiguration()
+        conf.getAttributes().put("cleanup", cleanupPolicyAttribute)
+        repo.stop()
+        repo.update(conf)
+        repo.start()
+    } catch (e) {
+        log.info("Attaching policy fail")
+    }
+}
+
+createPolicy('npmCleanupPolicy')
+attachPolicy('npmCleanupPolicy', 'npm-hosted')
--- nexus/scripts/groovy/nuget.groovy
+++ nexus/scripts/groovy/nuget.groovy
@@ -13,3 +13,40 @@ repository.createNugetProxy("nuget-proxy","https://api.nuget.org/v3/index.json",
 // strictContentTypeValidation - Whether or not the Repository should enforce strict content types
 
 repository.createNugetGroup("nuget-group", ["nuget-hosted", "nuget-proxy"], BlobStoreManager.DEFAULT_BLOBSTORE_NAME)
+
+
+
+
+
+def createPolicy (nugetPolicy) {
+    try {
+        def policyStorage = container.lookup(CleanupPolicyStorage.class.getName())
+        def cleanupPolicy = policyStorage.newCleanupPolicy()
+        cleanupPolicy.setName(nugetPolicy)
+        cleanupPolicy.setNotes('')
+        cleanupPolicy.setMode('deletion')
+        cleanupPolicy.setFormat('nuget')
+        cleanupPolicy.setCriteria(['regex': '.*SNAPSHOT'])
+        policyStorage.add(cleanupPolicy)
+    } catch (e) {
+        log.info("Cleanup policy already exists, skipping...")
+    }
+
+}
+
+def attachPolicy (policyName, repositoryName) {
+    try {
+        def repo = repository.repositoryManager.get(repositoryName)
+        def cleanupPolicyAttribute = [policyName: [policyName].toSet()]
+        def conf = repo.getConfiguration()
+        conf.getAttributes().put("cleanup", cleanupPolicyAttribute)
+        repo.stop()
+        repo.update(conf)
+        repo.start()
+    } catch (e) {
+        log.info("Attaching policy fail")
+    }
+}
+
+createPolicy('nugetCleanupPolicy')
+attachPolicy('nugetCleanupPolicy', 'nuget-hosted')
