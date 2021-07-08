776720f8bffdc2ef6ba9cfb759ea9732d93ae89c
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-08 13:13:52 +0100

    change

--- nexus/scripts/groovy/docker.groovy
+++ nexus/scripts/groovy/docker.groovy
@@ -1,4 +1,5 @@
 import org.sonatype.nexus.blobstore.api.BlobStoreManager
+import org.sonatype.nexus.cleanup.storage.CleanupPolicyStorage
 
 // create hosted repo and expose via https to allow deployments
 repository.createDockerHosted('docker-internal', 5000, null) 
@@ -20,5 +21,38 @@ repository.createDockerProxy('docker-hub',                   // name
 def groupMembers = ['docker-hub', 'docker-internal']
 repository.createDockerGroup('docker-all', null, 18443, groupMembers, true)
 
+
+def createPolicy (dockerPolicy) {
+    try {
+        def policyStorage = container.lookup(CleanupPolicyStorage.class.getName())
+        def cleanupPolicy = policyStorage.newCleanupPolicy()
+        cleanupPolicy.setName(dockerPolicy)
+        cleanupPolicy.setNotes('')
+        cleanupPolicy.setMode('deletion')
+        cleanupPolicy.setFormat('docker')
+        cleanupPolicy.setCriteria(['regex': '.*SNAPSHOT'])
+        policyStorage.add(cleanupPolicy)
+    } catch (e) {
+        log.info("Cleanup policy already exists, skipping...")
+    }
+
+}
+
+def attachPolicy (dockerPolicy, docker) {
+    try {
+        def repo = repository.repositoryManager.get(docker)
+        def cleanupPolicyAttribute = [dockerPolicy: [dockerPolicy].toSet()]
+        def conf = repo.getConfiguration()
+        conf.getAttributes().put("cleanup", cleanupPolicyAttribute)
+        repo.stop()
+        repo.update(conf)
+        repo.start()
+    } catch (e) {
+        log.info("Attaching policy fail")
+    }
+}
+
+createPolicy('dockerCleanupPolicy')
+attachPolicy('dockerCleanupPolicy', 'docker-docker')
 
 log.info('Script dockerRepositories completed successfully')
