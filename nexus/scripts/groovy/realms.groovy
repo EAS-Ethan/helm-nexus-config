dfa5009161070dcd03fc6b056e96fbc9d4c22063
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-08 16:34:22 +0100

    change

--- nexus/scripts/groovy/realms.groovy
+++ nexus/scripts/groovy/realms.groovy
@@ -2,6 +2,7 @@ import groovy.json.JsonSlurper
 import org.sonatype.nexus.security.realm.RealmManager
 
 realmManager = container.lookup(RealmManager.class.getName())
-
 // enable/disable the NuGet API-Key Realm
-realmManager.enableRealm("NuGetApiKey", parsed_args.nuget_api_key_realm)
+realmManager.enableRealm("NuGetApiKey")
+realmManager.enableRealm("DockerBearerToken")
+realmManager.enableRealm("npmBearerToken")
