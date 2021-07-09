0c1c1049dda8845051afbed23aff97b70fb8805d
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-07-09 14:26:17 +0100

    change

--- nexus/scripts/shell/provision.sh
+++ nexus/scripts/shell/provision.sh
@@ -11,6 +11,7 @@ host=http://localhost:8081
 function addAndRunScript {
   name=$1
   file=$2
+  
   /root/.sdkman/candidates/groovy/current/bin/groovy -Dgroovy.grape.report.downloads=true -Dgrape.config=/config/helpers/grapeConfig.xml /config/helpers/addUpdateScript.groovy -u "$username" -p "$password" -n "$name" -f "$file" -h "$host"
   printf "\nPublished $file as $name\n\n"
   curl -v -X POST -u $username:$password --header "Content-Type: text/plain" "$host/service/rest/v1/script/$name/run"
@@ -19,7 +20,7 @@ function addAndRunScript {
 
 printf "Provisioning Integration API Scripts Starting \n\n" 
 printf "Publishing and executing on $host\n"
-
+sleep 20000
 for f in /config/scripts/*.groovy; do
     s=${f%.*}
     filename=${s##*/}
